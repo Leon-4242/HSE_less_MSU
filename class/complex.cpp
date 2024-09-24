a9c05c8fb307d5ec0b071bd575c850fec60672e1
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-09-24 12:21:54 +0300

    complex upd

--- class/complex.cpp
+++ class/complex.cpp
@@ -1,50 +1,24 @@
 #include "complex.h"
+namespace comp {
+	Complex operator+ (const Complex& z1, const Complex& z2) {
+		return Complex(z1.real()+z2.real(), z1.imag() + z2.imag()); 
+	}
 
-Complex& Complex::operator= (const Complex& z){
-	x = z.x; y = z.y;
-	return *this;	
-}
+	Complex operator- (const Complex& z1, const Complex& z2) {
+		return Complex(z1.real()-z2.real(), z1.imag() - z2.imag());
+	}
+	
+	Complex operator* (const Complex& z1, const Complex& z2) {
+		return Complex(z1.real()*z2.real()-z1.imag()*z2.imag(), z1.real()*z2.imag()+z1.imag()*z2.real());
+	}
 
-Complex Complex::operator+ (const Complex& z) {
-	return Complex(x+z.x, y+z.y);
-}
-Complex& Complex::operator+= (const Complex& z) {
-	x += z.x;
-	y += z.y;
-	return *this;
-}
+	Complex operator/ (const Complex& z1, const Complex& z2) {
+		return z1*rev(z2);			
+	}
 
-Complex Complex::operator- (const Complex& z) {
-	return Complex(x-z.x, y-z.y);
-}
-Complex& Complex::operator-= (const Complex& z) {
-        x -= z.x;
-        y -= z.y;
-        return *this;
-}
-Complex Complex::operator- () {
-	return Complex(-x, -y);
+	Complex operator- (const Complex& z) {
+		return Complex(-z.real(), -z.imag());
+	}
 }
 
-Complex Complex::operator* (const Complex& z) {
-	return Complex(x*z.x-y*z.y, x*z.y+y*z.x);	
-}
-Complex& Complex::operator*= (const Complex& z) {
-	double tmp = x;
-        x = x*z.x-y*z.y;
-	y = tmp*z.y+y*z.x;
-	return *this;	
-}
-
-Complex Complex::operator* (double c) {
-	return Complex(x*c, y*c);
-}
-Complex& Complex::operator*= (double c) {
-	x *= c;
-	y *= c;
-	return *this;
-}
-Complex Complex::conj(){
-	return Complex(x, -y);	
-}
 
--- class/complex.h
+++ class/complex.h
@@ -3,53 +3,81 @@
 
 #include <math.h>
 
-constexpr double eps = 1e-12;
+namespace comp {
 
-class Complex {
-	double x, y;
-	
-	public:
-	Complex():
-       		x(0.),
-		y(0.)
-	{ }
+	constexpr double eps = 1e-12;
 
-	Complex(double a, double b):
-       		x(a), 
-		y(b)	
-	{ }
+	class Complex;
 
-	Complex(const Complex& z):
-		x(z.x),
-		y(z.y)
-	{ }
-
-	~Complex() { };
-
-	Complex& operator= (const Complex& z);
-
-	Complex operator+ (const Complex& z);
-	Complex& operator+= (const Complex& z);
-
+	Complex operator+ (const Complex& z1, const Complex& z2);
+	Complex operator- (const Complex& z1, const Complex& z2);
+	Complex operator* (const Complex& z1, const Complex& z2);
+	Complex operator/ (const Complex& z1, const Complex& z2);
 	Complex operator- (const Complex& z);
-	Complex& operator-= (const Complex& z);
-	Complex operator- ();
 
-	Complex operator* (const Complex& z);
-	Complex& operator*= (const Complex& z);
-
-	Complex operator* (double c);
-	Complex& operator*= (double c);
-
-	double mod() {
-		return sqrt(x*x+y*y);
+	Complex rev (const Complex& z) {
+		z1 = conj(z);
+		if (mod(z1) < eps) {
+			exit(-1);
+		}
+		return Complex(z1.real()/mod(z1), z1.imag()/mod(z1));
 	}
 
-	double arg() {
-		return(acos(x/(this->mod())));
+	Complex conj (const Complex& z) {
+		return Complex(z.real, -z.imag);
 	}
 
-	Complex conj();
-};
+	double mod(const Complex& z) {
+        	return sqrt(z.real()*z.real()+z.imag()*z.imag());
+	}
 
+        double arg(const Complex& z) {
+		if (mod(z) < eps) {
+			return 0;
+		}
+                return(acos(z.real()/(mod(z))));
+	}
+
+
+	class Complex {
+		double x, y;
+	
+		public:
+		Complex(): x(0.), y(0.) {}
+			
+		Complex(double a, double b = 0): x(a), y(b) {}
+	
+		Complex(const Complex& z): x(z.x), y(z.y) {}
+
+		~Complex() {};
+		
+		double real(void) const {
+			return x;
+		}
+
+		double imag(void) const {
+			return y;
+		}
+			
+		Complex& operator= (const Complex& z) {
+			x = z.x; y = z.y;
+			return *this;
+		}
+
+		Complex& operator+= (const Complex& z) {
+			*this = *this + z;
+			return *this;
+		}
+
+		Complex& operator-= (const Complex& z) {
+			*this = *this - z;
+			return *this;
+		}
+	
+		Complex& operator*= (const Complex& z) {
+			*this = (*this)*z;
+			return *this;
+		}
+	};
+}
 #endif
