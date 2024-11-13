271154fe087870ae8492ba509a0ffd765855cfd6
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-11-13 14:02:09 +0300

    update LIB

--- class/LIB.cpp
+++ class/LIB.cpp
@@ -1,4 +1,34 @@
-#include "complex.h"
+#include "LIB.h"
+
+namespace EXCEPT {
+	const char* Except::what() const noexcept {
+		return message.c_str();
+	}
+
+	Except::Except(const std::string text): message(text) { }
+
+	Except::~Except() {	}
+    
+}
+
+namespace CMP {
+	int cmp (const std::string str1, const std::string str2) {
+        if (cmplen(str1, str2) != 0) return cmplen(str1, str2);
+        if (str1 > str2) return 1;
+        if (str1 < str2) return -1;
+
+        return 0;
+    }
+    
+    int cmplen (const std::string str1, const std::string str2) {
+        if (str1.length() < str2.length()) return -1;
+        if (str1.length() > str2.length()) return 1;
+        return 0;
+    }
+    
+}
+
+
 namespace comp {
 	Complex::Complex(const std::string str) {
 		size_t tmp = str.find("+");
@@ -119,5 +149,3 @@ namespace comp {
 		return y;
 	}
 }
-
-
