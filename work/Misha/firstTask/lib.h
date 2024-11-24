ef9c260e6525e5c04c4ffa84e16da022faeac4fe
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-11-24 18:25:13 +0300

    in progress

---
+++ work/Misha/firstTask/lib.h
@@ -0,0 +1,15 @@
+#ifndef SALARYbase
+#define SALARYbase
+
+#include "RBTREE.h"
+
+namespace SALARYBASE {
+    using namespace RBTREE;
+
+    class SalaryBase {
+        RBTree<std::string>
+    }
+}
+
+#endif
+
