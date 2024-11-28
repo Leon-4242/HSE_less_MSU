1a68d3ffda69697908575d8603e3f456aa4a064a
Leon <112927065+Leon-4242@users.noreply.github.com>
2024-11-28 19:43:14 +0300

    Update main.cpp

--- work/Misha/firstTask/main.cpp
+++ work/Misha/firstTask/main.cpp
@@ -1,25 +1,20 @@
-#include "SALARYBASE.hpp"
+#include "salarybase.h"
 
 using namespace SALARYBASE;
 
 int main(void) {
-    try {
-        SalaryBase base;
-        base["first"] = 20000;
-        SalaryBase base1(base), base2;
-        base["second"] = 1000;
-        SalaryBase base3(base + base2);
-        base1 += base2;
-        if (base1 == base3) {
-            base.remove("first");
-        }
-        
-        std::cout <<base << std::endl;
-        std::cout <<base << base1 << base2 <<std::endl;
-        base.print(std::cout, "second", "zzzzzzz") << std::endl;
-    } 
-    catch( const Except& m) {
-        std::cout <<m.what() << std::endl;
+   SalaryBase base;
+    base["first"] = 20000;
+    SalaryBase base1(base), base2;
+    base["second"] = 1000;
+    SalaryBase base3(base + base2);
+    base1 += base2;
+    if (base1 == base3) {
+        base.remove("first");
     }
+        
+    std::cout <<base << std::endl;
+    std::cout <<base << base1 << base2 <<std::endl;
+    base.print(std::cout, "second", "zzzzzzz") << std::endl;
     return 0;
 }
