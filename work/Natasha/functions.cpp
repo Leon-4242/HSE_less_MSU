6b506db8c228dad713e7a47fc418bfb0f9f02cbe
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-12-14 04:23:00 +0300

    upd

--- work/Natasha/functions.cpp
+++ work/Natasha/functions.cpp
@@ -1,58 +1,71 @@
 #include <iostream>
-#include "vertex.h"
-#include "polygon.h"
-#include <vector>
 #include <cmath>
+#include "polygon.h"
 
-vertex vert; // вершина
-vert.vertex(double x, double y) : x(x), y(y) {}
-
-polygon pol; // многоугольник
-
-void vert.print_as_pair() { // вывод вершины на экран
-	cout << x << ", " << y << endl;
+void polygon::vertex::print_as_pair(void) const{ // вывод вершины на экран
+    std::cout << x << ", " << y << std::endl;
 }
 
-void pol.create(vector<vertex> newvertices) { // создать многоугольник
-	vertices = newvertices;
+void polygon::print_as_list(void) const { // вывести pol на экран в виде списка вершин
+    if (vertices.empty() == false) for (size_t i = 0; i < vertices.size(); ++i) vertices[i].print_as_pair();
+    else std::cout << "polygon is blank" << std::endl;
 }
 
-void pol.remove() { // удалить многоугольник
-	vertices.clear();
-} 
+bool polygon::insect_sides(const polygon& p) const {
+    for (size_t i = 0; i < vertices.size(); ++i) {
+        for (size_t j = 0; j < p.count_vertices(); ++j) {
+            if (insect_seg_seg(
+                segment(vertices[i], vertices[(i == vertices.size()-1) ? (0) : (i+1)]), 
+                segment(p[j], p[(j == p.count_vertices()-1) ? (0) : (j+1)])))
+            return true;
+        }
+    }
 
-int pol.count_vertices() { // посчитать количество вершин pol
-	return vertices.size();
+    return false;
 }
 
-void pol.print_as_list(pol) { // вывести pol на экран в виде списка вершин
-	if (vertices.empty == FALSE) {
-		int n = pol.count_vertices;
-		for (const auto& vert : vertices {
-			vert.print_as_pair()
-		}
-	}
-	else {
-		cout << "polygon is blank" << endl;
-	}
+size_t polygon::append_insects(std::vector<intersect>::iterator iter_insects, double y, bool flag) const{
+    size_t n = 0;
+    for (size_t i = 0; i < vertices.size(); ++i) {
+        vertex p1 = vertices[i];
+        vertex p2 = vertices[(i == vertices.size()-1) ? (0) : (i+1)];
+        if (p2 < p1) {
+            auto tmp(p1);
+            p1 = p2;
+            p2 = tmp;
+        }
+
+        if (p1.y < y && y < p2.y) {
+            (*(iter_insects+n)).f = flag;
+            (*(iter_insects+n)).n = p2.x * (y - p1.y) + p1.x * (p2.y - y);
+            (*(iter_insects+n)).d = p2.y - p1.y;
+            ++n;
+        }
+    }
+    return n;
 }
-	
-bool check_coverage(keypol, pol) // проверить вложение keypol в pol (метод суммирования углов)
-	int k = keypol.count_vertices;
-	int n = pol.count_vertices;
-	double angle_summ = 0.0;
-	for (int i = 0; i < k; i++) {
-		vertex vert = {keypol[i].x, keypol[i].y}
-		for (int j = 0; j < n; j++):
-			vertex a = {pol[j].x - vert.x, pol[j].y - vert.y};
-			vertex b = {pol[(j + 1) % n].x - vert.x, pol[pol[(j + 1) % n].y - vert.y};
-			double prod1 = a.x * b.y - a.y * b.x;
-			double prod2 = a.x * b.x + a.y * b.y;
-            double angle = atan2(prod1, prod2);
-            angle_summ += angle;
-		if (fabs(angle_summ) < 1e-9) == FALSE {
-			return FALSE;
-		}
-	}
-	return TRUE;
+
+bool polygon::contains_in_stripe(const polygon& keypol, double y, std::vector<intersect>& insects) const {
+    size_t n = append_insects(insects.begin(), y, true);
+    n += keypol.append_insects(insects.begin()+n, y, false);
+    sort(insects.begin(), insects.end());
+    bool f1 = false, f2 = false;
+    for (size_t i = 0; i < n; ++i) {
+        if (insects[i].f) f1 = (!f1 && insects[i].f) || (f1 && !insects[i].f);
+        else f2 = (!f2 && insects[i].f) || (f2 && !insects[i].f);
+        if (!f1 && f2) return false;
+    }
+    return true;
+}
+
+bool polygon::check_coverage(const polygon& keypol) const {// проверить вложение keypol в pol (метод суммирования углов)
+    if (insect_sides(keypol)) return false;
+    std::vector<vertex> points_sort_y(vertices); points_sort_y.insert(points_sort_y.end(), keypol.vertices.begin(), keypol.vertices.end());
+    sort(points_sort_y.begin(), points_sort_y.end());
+    std::vector<intersect> insects(points_sort_y.size());
+    for (size_t i = 1; i < points_sort_y.size(); ++i) {
+        double y = (points_sort_y[i-1].y + points_sort_y[i].y)/2;
+        if (!this->contains_in_stripe(keypol, y, insects)) return false;
+    }
+    return true;
 }
--- work/Natasha/polygon.h
+++ work/Natasha/polygon.h
@@ -2,18 +2,58 @@
 
 #pragma once
 
-#include "vertex.h"
 #include <vector>
+#include <algorithm>
 
 class polygon {
-	private:
-		vector<vertex> vertices; // массив из вершин
-	public:
-		void create(vector<vertex> newvertices); // создать многоугольник
-		void remove(); // удалить многоугольник
-		int count_vertices; // посчитать количество вершин многоугольника
-		void print_as_list; // вывести многоугольник на экран в виде списка вершин
-		bool check_vertex(vertex vert); // проверить вложение vert в данный многоугольник
-		bool check_all_vertices(polygon keypol); // проверить вложение всех вершин keypol в данный многоугольник
-		bool check_coverage(polygon keypol); // проверить вложение keypol в данный многоугольник
-}		
+    // class vertex
+    class vertex {
+        public:
+        double x, y; //coordinates
+        vertex (double x = 0, double y = 0) : x(x), y(y) {} //constructor
+        ~vertex(void) {} //destructor
+        vertex (const vertex& v): x(v.x), y(v.y) {} //copy constuctor
+        vertex& operator= (const vertex& v) {x = v.x; y= v.y; return *this;} //copy operator
+        bool operator< (const vertex& v) {return y < v.y;} // compare by y
+        void print_as_pair(void) const; // вывод вершины на экран
+        friend double area(const vertex& A, const vertex& B, const vertex& C) {return (B.x-A.x)*(C.y-A.y)-(B.y-A.y)*(C.x-A.x);} //area with sign
+    };
+
+    //class segment
+    class segment {
+        public:
+        vertex a, b; //border points
+        segment(vertex aa, vertex bb): a(aa), b(bb) {} //constuctor
+        ~segment(void) {} //destructor
+        segment(const segment& s): a(s.a), b(s.b) {} //copy constuctor
+        segment& operator= (const segment& s) {a = s.a; b = s.b; return *this;} //copy operator
+        friend bool insect_line_seg(const segment& line, const segment& s) {return area(line.a, line.b, s.a)*area(line.a, line.b, s.b) < 0;} //true <=> line intersects segment
+        friend bool insect_seg_seg(const segment& s1, const segment& s2) {return insect_line_seg(s1, s2) && insect_line_seg(s2, s1);} //true <=> segments have same point
+    };
+
+    class intersect {
+        public:
+        bool f; // if (first polygon) true else false
+        double d; // intersection point numerator
+        double n; // intersection point denominator
+        bool operator< (const intersect& in) const {return n*in.d<d*in.n;}
+    };
+
+    std::vector<vertex> vertices; // массив из вершин
+
+    public:
+        polygon(std::vector<vertex> newvertices): vertices(newvertices) {} // создать многоугольник
+        ~polygon(void) {vertices.clear();} // удалить многоугольник
+
+        size_t count_vertices(void) const {return vertices.size();} // посчитать количество вершин многоугольника
+        bool empty(void) const {return vertices.empty();}
+
+        const vertex& operator[] (const size_t& k) const {return vertices[k];}
+        vertex& operator[] (const size_t& k) {return vertices[k];}
+        void print_as_list(void) const; // вывести многоугольник на экран в виде списка вершин
+
+        bool insect_sides(const polygon& p) const;
+        size_t append_insects(std::vector<intersect>::iterator iter_insects, double y, bool flag) const;
+        bool contains_in_stripe(const polygon& keypol, double y, std::vector<intersect>& insect) const;
+        bool check_coverage(const polygon& keypol) const; // проверить вложение keypol в данный многоугольник
+};
