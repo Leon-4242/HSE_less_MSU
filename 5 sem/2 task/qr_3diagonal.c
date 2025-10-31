583c571c17f8832b1f356f849b1450e1f4867fc5
Leon-4242 <leon.zazovskii@math.msu.ru>
2025-10-31 05:25:04 +0300

    final

--- 5 sem/2 task/qr_3diagonal.c
+++ 5 sem/2 task/qr_3diagonal.c
@@ -1,6 +1,7 @@
 #include "qr_3diagonal.h"
 #include "output.h"
 
+int min(int a, int b);
 
 int qr_3diagonal(int n, double* a, double* res, double eps, double* t1, double* t2, int* its) {
 //page 67 - to 3diagonal by round
@@ -9,7 +10,7 @@ int qr_3diagonal(int n, double* a, double* res, double eps, double* t1, double* 
     long long start_us, end_us;
 	int i = 0, j = 0, k = 0, iter = 0, m = 0, t = 0;
 	double cos = 0, sin = 0, tmp = 0, norm_A = 0, sum = 0, 
-		   s_k = 0, norm_a1 = 0, prod = 0, norm_x = 0;
+		   s_k = 0, norm_a1 = 0, prod = 0, norm_x = 0, s = 0;
 	double x_k[2] = {0, 0}, x_old[2] = {0, 0};
 
 	m = n;
@@ -32,6 +33,7 @@ int qr_3diagonal(int n, double* a, double* res, double eps, double* t1, double* 
 		 */
 		for (i = k+2; i < n; ++i) {
 			tmp = sqrt(a[(k+1)*n+k]*a[(k+1)*n+k] + a[i*n+k]*a[i*n+k]);
+			if (fabs(tmp) < 1e-15) continue;
 			cos = a[(k+1)*n+k]/tmp; sin = - a[i*n+k]/tmp;
 
 			a[i*n+k] = 0;
@@ -42,19 +44,25 @@ int qr_3diagonal(int n, double* a, double* res, double eps, double* t1, double* 
 				a[(k+1)*n+j] = tmp;
 			}
 
-			//k+1
-			tmp = a[(k+1)*n+(k+1)]*cos-a[(k+1)*n+i]*sin;
-			a[(k+1)*n+i] = a[(k+1)*n+(k+1)]*sin+a[(k+1)*n+i]*cos;
-			a[(k+1)*n+(k+1)] = tmp;
-
-			//i
-			tmp = a[i*n+(k+1)]*cos-a[i*n+i]*sin;
-			a[i*n+i] = a[i*n+(k+1)]*sin+a[i*n+i]*cos;
-			a[i*n+(k+1)] = tmp;
-
+			for (j = k+1; j < n; ++j) {
+				if (j == k+1) {
+					tmp = a[(k+1)*n+(k+1)]*cos-a[(k+1)*n+i]*sin;
+					a[(k+1)*n+i] = a[(k+1)*n+(k+1)]*sin+a[(k+1)*n+i]*cos;
+					a[(k+1)*n+(k+1)] = tmp;
+				}
+				else if (j == i) {
+					tmp = a[i*n+(k+1)]*cos-a[i*n+i]*sin;
+					a[i*n+i] = a[i*n+(k+1)]*sin+a[i*n+i]*cos;
+					a[i*n+(k+1)] = tmp;
+				} else {
+					a[j*n+i] = a[i*n+j];
+					a[j*n+(k+1)] = a[(k+1)*n+j];
+				}
+			}
 		}
 		a[k*n+(k+1)] = a[(k+1)*n+k];
 		for (j = k+2; j < n; ++j) a[k*n+j] = 0;
+
 	}
 
 	gettimeofday(&end, NULL);
@@ -64,9 +72,9 @@ int qr_3diagonal(int n, double* a, double* res, double eps, double* t1, double* 
 
 	*t1 = (double)(end_us - start_us) / 1000000.;
 
-	printf("\n\nSym: \n");
-	output(n, n, n, a);
-	printf("\n");
+//	printf("\n\nSym: \n");
+//	output(n, n, n, a);
+//	printf("\n");
 
 	gettimeofday(&start, NULL);
 
@@ -75,77 +83,72 @@ int qr_3diagonal(int n, double* a, double* res, double eps, double* t1, double* 
 			s_k = a[(m-1)*n+(m-1)];
 			for (i = 0; i < m; ++i) {a[i*n+i] -= s_k;}
 			
-			for (k = 0; k < m-1; ++k) {
-				norm_a1 = sqrt(a[k*n+k]*a[k*n+k] + a[(k+1)*n+k]*a[(k+1)*n+k]);
-				
-				x_k[0] = a[k*n+k] - norm_a1;
-				x_k[1] = a[(k+1)*n+k];
-				
-				printf("\nx_k[0] = %e; x_k[1] = %e\n", x_k[0], x_k[1]);				
-				
-				if (fabs(norm_a1) < 1e-15) {
-					gettimeofday(&end, NULL);
- 
-				    start_us = start.tv_sec * 1000000 + start.tv_usec;
-				    end_us = end.tv_sec * 1000000 + end.tv_usec;
- 
-				    *t2 = (double)(end_us - start_us) / 1000000.;
-					return -1;
-				}
-				norm_x = sqrt(x_k[0]*x_k[0]+x_k[1]*x_k[1]);
-				if (fabs(norm_x) < 1e-15) continue;
+			for (k = 0; k < m; ++k) {
+				s = 0;
+				for (i = k+1; i < min(k+2, m); ++i) {s += a[i*n+k]*a[i*n+k];}
 
-				x_k[0] /= norm_x;
-				x_k[1] /= norm_x;
+				norm_a1 = sqrt(a[k*n+k]*a[k*n+k] + s);
+	
+				x_k[0] = a[k*n+k]-norm_a1;
+				if (k != m-1) x_k[1] = a[(k+1)*n+k];
+				else x_k[1] = 0;
 
-				printf("\nnorm_x_old = %e; x_k[0] = %e; x_k[1] = %e\n", norm_x, x_k[0], x_k[1]);		
+//				printf("\nx_k[0] = %e; x_k[1] = %e\n", x_k[0], x_k[1]);				
 				
-				a[k*n+k] = norm_a1;
-				a[(k+1)*n+k] = 0;
+				norm_x = sqrt(x_k[0]*x_k[0] + s);
+				if (!(fabs(norm_x) < 1e-15)) {
 
-					/*j = k+1*/
-				prod = x_k[0]*a[k*n+(k+1)] + x_k[1]*a[(k+1)*n+(k+1)];
-				prod *= 2;
-				a[k*n+(k+1)] -= prod*x_k[0];
-				a[(k+1)*n+(k+1)] -= prod*x_k[1];
+					x_k[0] /= norm_x;
+					if (k != m-1) {x_k[1] /= norm_x;}
+
+//					printf("\nnorm_x_old = %e; x_k[0] = %e; x_k[1] = %e\n", norm_x, x_k[0], x_k[1]);		
 				
-				if (k != m-2) {
-					prod = x_k[1]*a[(k+1)*n+(k+2)];
-					prod *= 2;
-					a[(k+1)*n+(k+2)] -= prod*x_k[1];
-				}
+					a[k*n+k] = norm_a1;
+					if (k != m-1) a[(k+1)*n+k] = 0;
+
+					for (j = k+1; j < min(k+3, m); ++j) {
+						if(j == k+2) prod = 0;
+						else prod = x_k[0]*a[k*n+j];
 					
-				if(k != 0) {
-					for (j = k-1; j < k+1 ; ++j) {
-						prod = x_old[0]*a[j*n+k-1] + x_old[1]*a[j*n+k];
+						for (t = k+1; t < min(k+2, m); ++t) {prod += x_k[t-k]*a[t*n+j];}
 						prod *= 2;
-						a[j*n+k-1] -= prod*x_old[0];
-						a[j*n+k] -= prod*x_old[1];
+					
+						for (t = k; t < min(k+2, m); ++t) {
+							if (j == k+2 && t == k) continue;
+							a[t*n+j] -= prod*x_k[t-k];
+						}
 					}
-
 				}
-
+				
+				if (k != 0) {
+					for (i = k-1; i < min(k+1, m); ++i) {
+						prod = x_old[0]*a[i*n+(k-1)];
+						for (t = k; t < min(k+1, m); ++t) {prod += x_old[t-(k-1)]*a[i*n+t];}
+						prod *= 2;
+						for (t = k-1; t < min(k+1, m); ++t) {a[i*n+t] -= prod*x_old[t-(k-1)];}
+					}
+					a[(k-1)*n+k] = a[k*n+(k-1)];
+				}
+	
 				x_old[0] = x_k[0]; x_old[1] = x_k[1];
 
-				if (k == m-2) {
-					for (j = m-2; j < m; ++j) {
-						prod = x_old[0]*a[j*n+k] + x_old[1]*a[j*n+(k+1)];
-						prod *= 2;
-						a[j*n+k] -= prod*x_old[0];
-						a[j*n+(k+1)] -= prod*x_old[1];
-					}
+				if (k == m-1) {
+					prod = 2*x_k[0]*a[(m-1)*n+(m-1)];
+					a[(m-1)*n+(m-1)] -= prod*x_k[0];
 				}
-				printf("\n\n k = %d\n", k);
-				output(n, n, n, a);
-				printf("\n");
+
+//				printf("\n\n k = %d\n", k);
+//				output(n, n, n, a);
+//				printf("\n");
+				
 			}
 
 			for (i = 0; i < m; ++i) {a[i*n+i] += s_k;}
 			++iter;
 
-			printf("\n\n iter = %d\n", iter);
-			output(n, n, n, a);
-			printf("\n");
+//			printf("\n\n iter = %d; m = %d\n", iter, m);
+//			output(n, n, n, a);
+//			printf("\n");
 		}
 		res[m-1] = a[(m-1)*n+(m-1)];
 		--m;
@@ -204,4 +207,8 @@ void r1_r2(int n, double* a, double* lambda, double* r1, double* r2) {
 	sum = sqrt(sum);
 
 	*r2 = fabs(sigma-sum)/norm_A;
+}
+
+int min(int a, int b) {
+	return (a < b ? a : b);
 }
