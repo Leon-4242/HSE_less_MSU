8da91bb5996d4e1170f29e11f0ca4696f9afe802
Leon-4242 <leon.zazovskii@math.msu.ru>
2025-12-07 23:53:38 +0300

    upd

--- work/Oleg/2 task/main.py
+++ work/Oleg/2 task/main.py
@@ -1,58 +1,89 @@
-from R2Graph import *
+from random import randint, shuffle
+from math import sqrt
 
-def minidisk(points):
-    if type(points) != list:
+def distance(a, b):
+    return sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2)
+
+
+def circle_from_2(a, b):
+    """Окружность, проходящая через две точки."""
+
+    center = [(a[i] + b[i]) / 2 for i in [0, 1]]
+    radius = distance(center, a)
+    return center, radius
+
+def circle_from_3(a, b, c):
+    """Окружность, проходящая через три точки (не коллинеарные)."""
+    # Формулы для центра описанной окружности
+    d = 2 * (
+        a[0] * (b[1] - c[1]) +
+        b[0] * (c[1] - a[1]) +
+        c[0] * (a[1] - b[1])
+    )
+
+    if abs(d) < 1e-14:  # коллинеарные точки
         return None
 
-    def circle(a, b, c):
-        A = a
-        B = b
-        C = c
-        AB = A.distance(B)
-        AC = A.distance(C)
-        BC = B.distance(C)
+    ux = (
+        (a[0]**2 + a[1]**2) * (b[1] - c[1]) +
+        (b[0]**2 + b[1]**2) * (c[1] - a[1]) +
+        (c[0]**2 + c[1]**2) * (a[1] - b[1])
+    ) / d
 
-        if AC > AB:
-            B, C = C, B
-            AC, AB = AB, AC
-        if BC > AB:
-            A, C = C, A
-            BC, AB = AB, BC
-        
-        d = [(A+B)*0.5, A.distance(B)/2]
-        if d[0].distance(C) <= d[1]:
-            return d
-        M = (A+C)*0.5
-        res = intersectLine(d, (A-B).normal(), M, (A-C).normal())
-        O = []
-        if res[0]:
-            O = res[1]
-        else:
-            O = None
+    uy = (
+        (a[0]**2 + a[0]**2) * (c[0] - b[0]) +
+        (b[0]**2 + b[0]**2) * (a[0] - c[0]) +
+        (c[0]**2 + c[0]**2) * (b[0] - a[0])
+    ) / d
 
-        return [O, O.distance(O, C)]
+    center = [ux, uy]
+    radius = distance(center, a)
+    return center, radius
 
-    def md(R):
+
+def is_inside(point, circle):
+    center, r = circle
+    return distance(point, center) <= r + 1e-12
+
+
+def welzl(points, R, n):
+    """Рекурсивная часть алгоритма Вейцеля."""
+    if n == 0 or len(R) == 3:
+        # Строим минимальную окружность по точкам R
         if len(R) == 0:
-            return [R2Point(0, 0), -1]
+            return None
         elif len(R) == 1:
-            return [R[0], 0]
+            return (R[0], 0.0)
         elif len(R) == 2:
-            return circle(R[0], R[1], (R[0]+R[1])*0.5)
-        return circle(R[0], R[1], R[2])
+            return circle_from_2(R[0], R[1])
+        else:
+            return circle_from_3(R[0], R[1], R[2])
 
-    def b_minidisk(P, R):
-        if len(P) == 0 or len(R) == 3:
-            return md(R)
+    # Случайная точка
+    idx = randint(0, n - 1)
+    p = points[idx]
 
-        p = P.pop()
-        D = b_minidisk(P, R)
-        if D[0].distance(p) > D[1]:
-            R.append(p)
-            D = b_minidisk(P, R)
-        return D
+    # Перемещаем выбранную точку в конец диапазона
+    points[idx], points[n - 1] = points[n - 1], points[idx]
 
-    return b_minidisk(points, [])
+    D = welzl(points, R, n - 1)
+
+    # Точка оказывается вне текущей окружности
+    if D is None or not is_inside(p, D):
+        R.append(p)
+        D = welzl(points, R, n - 1)
+        R.pop()
+
+    return D
+
+
+def minimal_enclosing_circle(points):
+    if not points:
+        return None
+
+    pts = points[:]
+    shuffle(pts)
+    return welzl(pts, [], len(pts))
 
 def main():
     points = []
@@ -62,14 +93,14 @@ def main():
         for i in range(n):
             buff = input()
             buff = list(map(float, buff.split()))
-            points.append(R2Point(buff))
+            points.append(buff)
     else:
         filename = input("Enter filename: ")
         with open(filename, "r") as file:
             for line in file:
-                points.append(R2Point(list(map(float, line.split()))))
+                points.append(list(map(float, line.split())))
 
-    print(minidisk(points))
+    print(minimal_enclosing_circle(points))
 
 if __name__ == "__main__":
     main()
